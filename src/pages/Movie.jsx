1204d03e9cf5f77d718c36abefaaf020cd15bd0f
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-05 19:56:17 -0300

    Segundo Commit

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import data from '../../data.json';
 
 export const Movie = () => {
   return (
-    <div>Movie</div>
+    <>
+      {
+        data.movies.map(movie => (
+          <Card key={movie.id} style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={movie.image} />
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.year}</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        ))
+      }
+    </>
   )
 }
